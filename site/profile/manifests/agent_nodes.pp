998c733e43b8ae43acb9b0c3bfb2dea5cf9491c6
dsctsg <58735790+dsctsg@users.noreply.github.com>
2019-12-10 13:24:47 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include docker
-  docker::node {'web.puppet.vm':}
-  docker::node {'db.puppet.vm':}
+  docker::image {'web.puppet.vm':}
+  docker::image {'db.puppet.vm':}
 }
