05392efd8d878e4192714268b8b94dedda3caf3a
dsctsg <58735790+dsctsg@users.noreply.github.com>
2019-12-11 09:14:34 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,16 @@
 class profile::agent_nodes {
   include dockeragent
+  # docker nodes
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
+  
+  # ssh nodes
+  host { 'web.puppet.vm':
+    ensure => present,
+    ip     => '172.18.0.2',
+  }
+  host { 'db.puppet.vm':
+    ensure => present,
+    ip     => '172.18.0.3',
+  }
 }
