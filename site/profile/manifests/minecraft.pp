e1f8f697a5e82579551465bd275963ed8d4e7014
dsctsg <58735790+dsctsg@users.noreply.github.com>
2019-12-11 14:36:35 -0500

    Update minecraft.pp

--- site/profile/manifests/minecraft.pp
+++ site/profile/manifests/minecraft.pp
@@ -1,3 +1,8 @@
 class profile::minecraft {
-  include minecraft
+  #include minecraft
+  class { 'minecraft':
+    # change installation directory on the fly, but this impact minecraft.service file
+    # use tempplate to resolve that
+    mc_dir => '/srv/minecraft',
+  }
 }
