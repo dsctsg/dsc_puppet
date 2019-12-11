f034eb15e8b04d26fd614bb94b0da56b1d2dcd27
dsctsg <58735790+dsctsg@users.noreply.github.com>
2019-12-11 10:23:24 -0500

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,24 @@
+class minecraft {
+  package { 'java':
+    ensure => present,
+  }
+  
+  file { '/opt/minecraft':
+    ensure => directory,
+  }
+  
+  file { '/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/e9f105b3c5c7e85c7b445249a93362a22f62442d/server.jar',
+  }
+  
+  file { '/opt/minecraft/eula.txt':
+    ensure  => file,
+    content => 'eula=true',
+  }
+  
+  file { '/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+}
